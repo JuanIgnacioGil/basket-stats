54f241bd5f2f65b9e3141fb7284036f61f1a644b
Juan Ignacio Gil Gómez <juanignacio@gilgomez.net>
2018-12-25 13:17:56 +0100

    Properly retrieving tweets dataset

--- sentiment_analysis/data_collection.py
+++ sentiment_analysis/data_collection.py
@@ -266,22 +266,22 @@ def get_all_tweet(api, final_tw_account):
         creat_time_list = []
 
         for twt in tweepy.Cursor(api.user_timeline, id=ID, tweet_mode='extended').items(1000):
-            if twt.created_at > datetime.datetime(2018, 11, 1, 0, 0, 0):  # extract time after
+            if twt.created_at > datetime.datetime(2016, 11, 1, 0, 0, 0):  # extract time after
                 try:
                     tweet_list.append(twt.retweeted_status.full_text)
                 except AttributeError:
                     tweet_list.append(twt.full_text)
                 creat_time_list.append(twt.created_at)
 
-    all_nba_tweet += tweet_list
-    all_creat_time += creat_time_list
-    followers.append(twt.user.followers_count)
-    friends.append(twt.user.friends_count)
-    favourites.append(twt.user.favourites_count)
+        all_nba_tweet.append(tweet_list)
+        all_creat_time.append(creat_time_list)
+        followers.append(twt.user.followers_count)
+        friends.append(twt.user.friends_count)
+        favourites.append(twt.user.favourites_count)
 
     df_tweet = pd.DataFrame.from_records(
-        zip(all_nba_tweet, all_creat_time, followers, friends, favourites),
-        columns=["Tweets", "Create_time", "Followers", "Friends", "Favourites"]
+        zip(all_nba_tweet, all_creat_time, followers, friends, favourites, final_tw_account.keys()),
+        columns=["Tweets", "Create_time", "Followers", "Friends", "Favourites", 'Name']
     )
 
     return df_tweet
