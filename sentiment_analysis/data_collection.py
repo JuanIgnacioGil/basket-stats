04dbbd6c367f3b9db8dbdc9728fe31f9c7292800
Juan Ignacio Gil Gómez <juanignacio@gilgomez.net>
2019-02-17 20:22:41 +0000

    WIP: Retrieving all tweets

--- sentiment_analysis/data_collection.py
+++ sentiment_analysis/data_collection.py
@@ -265,22 +265,34 @@ def get_all_tweet(api, final_tw_account):
     favourites = []
 
     for ID in final_tw_account.values():
+        print(ID)
         tweet_list = []
         creat_time_list = []
 
-        for twt in tweepy.Cursor(api.user_timeline, id=ID, tweet_mode='extended').items(1000):
-            if twt.created_at > datetime.datetime(2016, 11, 1, 0, 0, 0):  # extract time after
-                try:
-                    tweet_list.append(twt.retweeted_status.full_text)
-                except AttributeError:
-                    tweet_list.append(twt.full_text)
-                creat_time_list.append(twt.created_at)
+        try:
+            id_tweets = tweepy.Cursor(api.user_timeline, id=ID, tweet_mode='extended').items(1000)
+        except:
+            id_tweets = []
 
-        all_nba_tweet.append(tweet_list)
-        all_creat_time.append(creat_time_list)
-        followers.append(twt.user.followers_count)
-        friends.append(twt.user.friends_count)
-        favourites.append(twt.user.favourites_count)
+        first = next(id_tweets, None)
+
+        if first is not None:
+            for twt in id_tweets:
+                if twt.created_at > datetime.datetime(2016, 11, 1, 0, 0, 0):  # extract time after
+                    try:
+                        tweet_list.append(twt.retweeted_status.full_text)
+                    except:
+                        tweet_list.append(twt.full_text)
+
+                    creat_time_list.append(twt.created_at)
+
+            all_nba_tweet.append(tweet_list)
+            all_creat_time.append(creat_time_list)
+            followers.append(twt.user.followers_count)
+            friends.append(twt.user.friends_count)
+            favourites.append(twt.user.favourites_count)
+        else:
+            print('Error:', ID)
 
     df_tweet = pd.DataFrame.from_records(
         zip(all_nba_tweet, all_creat_time, followers, friends, favourites, final_tw_account.keys()),
